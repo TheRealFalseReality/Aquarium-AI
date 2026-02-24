54d36ba47100ae2b466486196aef1de6564d3eaf
Nicholas White <nicholaswhite@capitalcityaquatics.com>
2026-02-24 00:54:54 -0500

    Update assets and bump version to 3.0.01
    Refresh AquaPiEssentials.jpg and AquaPiMainSmaller.jpg, remove obsolete AquaPiEssentials.png, and increment app version in pubspec.yaml from 3.0.00+3.0.00 to 3.0.01+3.0.01. These changes update visual assets and apply a patch version bump.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import firebase_core
 import firebase_crashlytics
 import firebase_remote_config
 import flutter_local_notifications
+import in_app_purchase_storekit
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -32,6 +33,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
